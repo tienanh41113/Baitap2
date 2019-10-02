a0c74fc91ade6557e33cb92f500c3b6386ee988b
Tien Anh <tienanh41161@mail.com>
2019-10-02 15:09:15 +0700

    Add project files.

---
+++ songuyento/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace songuyento
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int[] mang = {7, 5, 6, 9, 3}; 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }  
+        private void btntong_Click(object sender, RoutedEventArgs e)
+        {
+            int s = 0;
+            foreach (int i in mang)
+                s += i;
+            txtkq.Text = "Tổng = " + s.ToString();
+        }
+
+        private void btntongle_Click(object sender, RoutedEventArgs e)
+        {
+            int s = 0;
+            foreach (int i in mang)
+                if (i % 2 != 0)
+                    s += i;
+            txtkq.Text = "Tổng lẻ = " + s.ToString();
+        }
+    }
+}
